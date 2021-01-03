4e5f3d91bdf449be3a1114e6352dd40bfe63fd5b
Justin Joyce <justin.joyce@joocer.com>
2021-01-03 21:16:21 +0000

    Update trosi.py

--- trosi.py
+++ trosi.py
@@ -25,20 +25,20 @@ except:
     import json
 
 
+def safe_get(arr, index, default=None):
+    return arr[index] if index < len(arr) else default
+
 def get_input_stream():
     """
     If first paramter is - use standard in, if it's a existing
     file, open an use that
     Return a stream (or None) for processing.
-    """
-                                      
-    if len(sys.argv) > 1:
-        input_filename = sys.argv[1]
-        if input_filename == '-':
-            if not sys.stdin.isatty():
-                return sys.stdin    
-        if os.path.isfile(input_filename):
-            return open(input_filename, 'r')
+    """   
+    if input_filename == '-':
+        if not sys.stdin.isatty():
+            return sys.stdin    
+    if os.path.isfile(input_filename):
+        return open(input_filename, 'r')
     return None
 
 def get_parameter_value(label):
@@ -49,8 +49,7 @@ def get_parameter_value(label):
     """
     if label in sys.argv:
         idx = sys.argv.index(label)
-        if (idx + 1) < len(sys.argv):
-            return sys.argv[idx + 1]
+        return safe_get(sys.argv, idx + 1)
     return None
 
 # Initialize variables
@@ -59,16 +58,18 @@ verbose      = '--verbose' in sys.argv or '-v' in sys.argv
 show_help    = '-?' in sys.argv or '-h' in sys.argv or '--help' in sys.argv
 no_headers   = '-no' in sys.argv
 out_file     = get_parameter_value('-o')
+input_filename = safe_get(sys.argv, 1)
 input_stream = get_input_stream()
 
 # if help requested, display help and exit with no error
 if show_help:
-    print("Usage: trosi [FILE] [-o OUTPUTFILE] [--help]")
+    print("Usage: trosi [FILE] [-o OUTPUTFILE] [--verbose] [--help]")
     print("convert INPUTFILE to a jsonl formatted file")
     print("Example: trosi data.csv -o data.jsonl")
     print()
     print("  FILE\t\tfile to convert")
     print("  -o\t\tfile to save to")
+    print("  -v, --verbose\tflag to increase the amount of logging")
     print("  -h, --help\tdisplay this help text and exit")
     print()
     print("When FILE is -, standard input in read.")
@@ -77,13 +78,13 @@ if show_help:
 # if we have nothing to processes, display an error and how to get help
 if not input_stream:
     print("No input specified.")
-    print(f"Try '{sys.argv[0]} --help' for usage information.")
+    print(f"Try '{safe_get(sys.argv, 0)} --help' for usage information.")
     sys.exit(1)
 
 # if we have nothing to processes, display an error and how to get help
 if not out_file:
     print("No output file specified.")
-    print(f"Try '{sys.argv[0]} --help' for usage information.")
+    print(f"Try '{safe_get(sys.argv, 0)} --help' for usage information.")
     sys.exit(1)
 
 with open(out_file, 'w', encoding='utf8') as of:
